c7b948f48abe30972d378053cc460e11138292a5
ccyang617 <156895081+ccyang617@users.noreply.github.com>
2024-03-24 22:24:51 -1000

    ‘landing page mockup finished in 20 minutes.

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import { PeopleFill, FileEarmarkTextFill, Calendar2CheckFill } from 'react-bootstrap-icons';
 import { Col, Container, Image, Row } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <Container id="landing-page" fluid className="py-3">
+  <Container id="landing-page" className="py-3">
     <Row className="align-middle text-center">
-      <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
+      <Col>
+        <PeopleFill size={100} />
+        <h4 className="body-text">This address book enables any number of users to register and save their business contacts. You can only see the contacts you have created.</h4>
       </Col>
 
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>Welcome to this template</h1>
-        <p>Now get to work and modify this app!</p>
+      <Col>
+        <FileEarmarkTextFill size={100} />
+        <h4 className="body-text">For each contact, you can save their name, address, and phone number.</h4>
+      </Col>
+      <Col>
+        <Calendar2CheckFill size={100} />
+        <h4 className="body-text">Each time you make contact with a contact, you can write a note that summarizes the conversation. This note is saved along with a timestamp with the contact.</h4>
       </Col>
 
     </Row>
